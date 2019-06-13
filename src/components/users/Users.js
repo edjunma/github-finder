2d7742885609e0b13cd46a9be285ffef24cd945e
edjunma <junwenma@buffalo.edu>
2019-06-13 12:01:33 -0400

    refactor Users, adding Spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-class Users extends Component {
-	render() {
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />;
+	} else {
 		return (
 			<div style={userStyle}>
 				{this.props.users.map(user => (
@@ -11,7 +14,7 @@ class Users extends Component {
 			</div>
 		);
 	}
-}
+};
 
 const userStyle = {
 	display: 'grid',
