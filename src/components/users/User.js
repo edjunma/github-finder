4ea266235075d2d18a7f4ef383a0368b664db8ba
edjunma <junwenma@buffalo.edu>
2019-06-14 21:33:29 -0400

    setting up user profile ui layout

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
 	}
+
+	static propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		getUser: PropTypes.func.isRequired
+	};
+
 	render() {
 		const {
 			name,
@@ -22,7 +32,39 @@ export class User extends Component {
 
 		const { loading } = this.props;
 
-		return <div>{name}</div>;
+		if (loading) return <Spinner />;
+
+		return (
+			<Fragment>
+				<Link to='/' className='btn btn-light'>
+					Back To Search
+				</Link>
+				Hireable:{' '}
+				{hireable ? (
+					<i className='fas fa-check text-success' />
+				) : (
+					<i className='fas fa-times-circle text-danger' />
+				)}
+				<div className='card grid-2'>
+					<div className='all-center'>
+						<img src='{avatar_url}' className='round-img' alt='' style={{ width: '150px' }} />
+						<h1>{name}</h1>
+						<p>Location: {location}</p>
+					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Bio</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+						<a href='{html_url}' className='btn btn-dark my-1'>
+							Visit GitHub Profile
+						</a>
+					</div>
+				</div>
+			</Fragment>
+		);
 	}
 }
 
