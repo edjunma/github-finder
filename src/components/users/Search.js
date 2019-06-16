625a5816a93bc2bf9b971a2617576577e3404994
edjunma <junwenma@buffalo.edu>
2019-06-16 19:41:15 -0400

    refactor Search class function with useState Hook

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
+const Search = ({ searchUsers, showClear, clearUsers }) => {
 	state = {
 		text: ''
 	};
 
-	static propTypes = {
-		searchUsers: PropTypes.func.isRequired,
-		clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-	};
-
-	onSubmit = e => {
+	const onSubmit = e => {
 		e.preventDefault();
 		if (this.state.text === '') {
 			this.props.setAlert('Please enter something', 'light');
@@ -23,25 +16,28 @@ export class Search extends Component {
 		}
 	};
 
-	onChange = e => this.setState({ [e.target.name]: e.target.value });
+	const onChange = e => this.setState({ [e.target.name]: e.target.value });
 
-	render() {
-		const { showClear, clearUsers } = this.props;
+	return (
+		<div>
+			<form onSubmit={this.onSubmit} className='form'>
+				<input type='text' name='text' placeholder='Search Users...' onChange={this.onChange} />
+				<input type='submit' value='Search' className='btn btn-dark btn-block' />
+			</form>
+			{showClear && (
+				<button className='btn btn-light btn-block' onClick={clearUsers}>
+					Clear
+				</button>
+			)}
+		</div>
+	);
+};
 
-		return (
-			<div>
-				<form onSubmit={this.onSubmit} className='form'>
-					<input type='text' name='text' placeholder='Search Users...' onChange={this.onChange} />
-					<input type='submit' value='Search' className='btn btn-dark btn-block' />
-				</form>
-				{showClear && (
-					<button className='btn btn-light btn-block' onClick={clearUsers}>
-						Clear
-					</button>
-				)}
-			</div>
-		);
-	}
-}
+Search.propTypes = {
+	searchUsers: PropTypes.func.isRequired,
+	clearUsers: PropTypes.func.isRequired,
+	showClear: PropTypes.bool.isRequired,
+	setAlert: PropTypes.func.isRequired
+};
 
 export default Search;
