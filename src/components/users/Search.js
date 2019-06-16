ea7e02433b032c43f0b58435727fdc63e5b34a3d
edjunma <junwenma@buffalo.edu>
2019-06-16 19:49:30 -0400

    useState hook to refactor state search class functions

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Search = ({ searchUsers, showClear, clearUsers }) => {
-	state = {
-		text: ''
-	};
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+	const [text, setText] = useState('');
 
 	const onSubmit = e => {
 		e.preventDefault();
-		if (this.state.text === '') {
-			this.props.setAlert('Please enter something', 'light');
+		if (text === '') {
+			setAlert('Please enter something', 'light');
 		} else {
-			this.props.searchUsers(this.state.text);
-			this.setState({ text: '' });
+			searchUsers(text);
+			setText('');
 		}
 	};
 
-	const onChange = e => this.setState({ [e.target.name]: e.target.value });
+	const onChange = e => setText(e.target.value);
 
 	return (
 		<div>
-			<form onSubmit={this.onSubmit} className='form'>
-				<input type='text' name='text' placeholder='Search Users...' onChange={this.onChange} />
+			<form onSubmit={onSubmit} className='form'>
+				<input
+					type='text'
+					name='text'
+					placeholder='Search Users...'
+					value={text}
+					onChange={onChange}
+				/>
 				<input type='submit' value='Search' className='btn btn-dark btn-block' />
 			</form>
 			{showClear && (
